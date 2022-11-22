9e855db58ac37f888eaff5de73954c58fddb0672
CodeElevator <codeelevator131@gmail.com>
2022-11-22 10:08:25 +0200

    Modified .gitignore

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-db = sqlite3.connect("DB.sqlite")
-cur = db.cursor()
-result = cur.execute("SELECT role FROM rolereact WHERE msg_id = ? AND emoji = ?",(1043988724574330880, str("🟦")
-))
-r = result.fetchall()
-for entry in r:
-    print(entry[0])
